b3d921e415538dd76d34adfcf228fde6c39ef9b1
Shivi127 <singhnight@gmail.com>
2020-12-11 12:06:54 -0500

    Adding host to the application to allow it to be accessed from outside

--- Stocks/startserver.sh
+++ Stocks/startserver.sh
@@ -8,10 +8,12 @@ sudo -H -u postgres bash -c "createdb -O stocksdb 'stocksdb'";
 export PGPASSWORD=stocksdb
 psql -h localhost -d stocksdb -U stocksdb -f /Stocks/sql/load_stocks_into_db.sql
 
+# Populate the DB with random stocks
+
 python3 /Stocks/stocks_scraper.py 
-# TODO: set up a working directory??
+
 export FLASK_RUN_PORT=8000
 export FLASK_APP=/Stocks/stocks_api
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
-flask run
+flask run -h 0.0.0.0
